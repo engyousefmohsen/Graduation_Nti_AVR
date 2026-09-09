ea947437a160ac6a8aa83877789d76d4c37e351f
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-09-10 01:47:33 +0300

    Update main.c

--- project/APP/main.c
+++ project/APP/main.c
@@ -10,6 +10,7 @@
 #include "../LIB/Std_Types.h"
 #include "../MCAL/DIO/DIO_int.h"
 #include "headlights_app.h"
+#include "smart_ac_app.h"
 
 /* TODO (team): call the Init/MainFunction of garage_app, movement_app,
  * and smart_ac_app here as well once each teammate finishes their part. */
@@ -17,11 +18,13 @@
 int main(void)
 {
 	DIO_voidInitialization();
-
+    SmartAC_voidInit();
 	HEADLIGHTS_voidInit();
 
 	while(1)
 	{
+		SmartAC_voidUpdate();
+
 		HEADLIGHTS_voidMainFunction();
 		_delay_ms(100);
 	}
