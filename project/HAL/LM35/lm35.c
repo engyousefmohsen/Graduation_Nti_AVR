76d176574fd71e60d3369da33b908ca2faf353cf
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-09-10 00:25:40 +0300

    Add LM35 initialization and temperature reading
    Implement LM35 temperature sensor functions.

---
+++ project/HAL/LM35/lm35.c
@@ -0,0 +1,14 @@
+#include "../../MCAL/ADC/ADC_Interface.h"
+#include"../../LIB/std_types.h"
+#include "lm35.h"
+void lm35_init(void) {
+    //it just need adc initialization which is done so it is here for structure only
+}
+
+
+f32 lm35_read_temp(void) {
+    u16 adc_value = ADC_Read(ADC_CHANNEL_0);
+    f32 voltage = (adc_value * VREF_MV) / ADC_RESOLUTION;
+    f32 temp_c = voltage / LM35_MV_PER_C;
+    return temp_c;
+}
