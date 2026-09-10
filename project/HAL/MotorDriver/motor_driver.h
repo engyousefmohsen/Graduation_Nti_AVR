12a32d1386c9235036e6c2900cdaa8f9c793b24b
Zain~San <117827268+zainELd33n@users.noreply.github.com>
2026-09-10 03:09:35 +0300

    Update motor_driver.h

--- project/HAL/MotorDriver/motor_driver.h
+++ project/HAL/MotorDriver/motor_driver.h
@@ -29,30 +29,4 @@ void motor_turnRight(u8 port);
 void motor_turnLeft(u8 port);
 void motor_increaseSpeed();
 
-#endif/*
- * motor drive.h
- *
- * Created: 9/8/2026
- * Author : Zain
- */ 
-
-#ifndef MOTORS_H
-#define MOTORS_H
-
-#include "../../MCAL/DIO/dio.h"
-#include "../../LIB/std_types.h"
-
-//sets the pins needed to control the motors through the motor driver
-void motor_init(u8 port);
-
-
-//is used when needed to stop motors for any reason
-void motor_stop(u8 port);
-
-//movement for the car
-void motor_moveForward(u8 port);
-void motor_moveBackward(u8 port);
-void motor_turnRight(u8 port);
-void motor_turnLeft(u8 port);
-
 #endif
