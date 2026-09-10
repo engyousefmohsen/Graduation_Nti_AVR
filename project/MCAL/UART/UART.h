94ec03f3e3c3004d5b245a6b3908795c4c7052c7
Zain~San <117827268+zainELd33n@users.noreply.github.com>
2026-09-10 15:46:30 +0300

    Add UART register definitions

--- project/MCAL/UART/UART.h
+++ project/MCAL/UART/UART.h
@@ -10,6 +10,27 @@
 
 #include "../../LIB/std_types.h"
 
+// UART Register Addresses for ATmega32
+#define UDR       *((volatile u8*)0x2C)
+#define UCSRA     *((volatile u8*)0x2B)
+#define UCSRB     *((volatile u8*)0x2A)
+#define UCSRC     *((volatile u8*)0x40)
+#define UBRRH     *((volatile u8*)0x40)
+#define UBRRL     *((volatile u8*)0x29)
+
+// Register Bit Definitions 
+#define RXC       7
+#define TXC       6
+#define UDRE      5
+#define U2X       1
+
+#define RXEN      4
+#define TXEN      3
+
+#define URSEL     7
+#define UCSZ1     2
+#define UCSZ0     1
+
 // Initializes the UART module with a given baud rate (e.g., 9600)
 void UART_init(u32 baud_rate);
 
