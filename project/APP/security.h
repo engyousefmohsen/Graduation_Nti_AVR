0c770f7dfc7a6c08bd301d4c05296d49e6dd2794
Abdelrahman Elbokhary <101464711+Albokhary1@users.noreply.github.com>
2026-09-10 13:16:24 +0300

    Create security.h for user authentication functions
    Added header file for login and password management system.

---
+++ project/APP/security.h
@@ -0,0 +1,15 @@
+/*
+ * security.h
+ * Author: Abdelrahman Elbokhary
+ * Description: Login and Password Management System
+ */ 
+
+#ifndef SECURITY_H_
+#define SECURITY_H_
+
+#include "../LIB/std_types.h"
+
+/* Function Prototypes */
+void Security_Login(void);
+
+#endif /* SECURITY_H_ */
