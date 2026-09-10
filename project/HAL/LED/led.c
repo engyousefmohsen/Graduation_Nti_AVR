95ccfd877534504e596c872937720cd317099c30
Yousef Mohsen <eng.yousefmohsen@gmail.com>
2026-09-10 18:12:25 +0300

    Update LED driver to use Timer2_PWM instead of Timer0

--- project/HAL/LED/led.c
+++ project/HAL/LED/led.c
@@ -2,17 +2,17 @@
  * led.c
  *
  * HAL Layer - LED / Lamp Driver Implementation
- * Built on top of MCAL/Timer0 (Fast PWM mode).
+ * Built on top of MCAL/Timer2_PWM (Fast PWM mode).
  */ 
 
 #include "../../LIB/Std_Types.h"
-#include "../../MCAL/Timer0/TIMER0_int.h"
+#include "../../MCAL/Timer2_PWM/timer2_pwm.h"
 
 #include "led.h"
 
 void LED_voidInit(void)
 {
-	TIMER0_voidInit();
+	TIMER2_voidInit();
 }
 
 void LED_voidSetIntensity(u8 Percentage)
@@ -24,7 +24,7 @@ void LED_voidSetIntensity(u8 Percentage)
 		Percentage = 100;
 	}
 
-	/* Map 0-100% to 0-255 (OCR0 range), then hand the raw value to MCAL */
+	/* Map 0-100% to 0-255 (OCR2 range), then hand the raw value to MCAL */
 	OcrValue = (u8)(((u16)Percentage * 255) / 100);
-	TIMER0_voidSetCompare(OcrValue);
+	TIMER2_voidSetCompare(OcrValue);
 }
