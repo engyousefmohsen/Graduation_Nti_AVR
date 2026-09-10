5a3257907a00dad3c2da40f621db384eeba895fc
Abdelrahman Elbokhary <101464711+Albokhary1@users.noreply.github.com>
2026-09-10 13:16:54 +0300

    Update Keypad.h

--- project/HAL/Keypad/Keypad.h
+++ project/HAL/Keypad/Keypad.h
@@ -1,1 +1,27 @@
 
+/*
+ * keypad.h
+ * Author: Abdelrahman Elbokhary
+ * Description: Header file for 4x4 Keypad Driver
+ */ 
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include "../../LIB/std_types.h" 
+
+/* Keypad Port Configuration on PORTC */
+#define KEYPAD_PORT_DIR   DDRC
+#define KEYPAD_PORT_OUT   PORTC
+#define KEYPAD_PORT_IN    PINC
+
+#define KEYPAD_NOT_PRESSED 0xFF
+
+/* Functions Prototypes */
+void Keypad_Init(void);
+u8 Keypad_GetPressedKey(void);
+
+#endif /* KEYPAD_H_ */
